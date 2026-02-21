db4a1d62eab3813d45c19553f72eeeda103f8369
sbsjonathan <jonjonathan2.0@gmail.com>
2026-02-21 02:04:06 -0300

    Atualizar o jw.js

--- jw.js
+++ jw.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("ok do hope (v1)", {
+    return new Response("ok do phope (v1)", {
       headers: { "content-type": "text/plain; charset=utf-8" },
     });
   },
